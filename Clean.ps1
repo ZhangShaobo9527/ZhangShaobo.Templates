f844baa6f95c3628bc3a3fca8d1743d4fcfb6449
ZhangShaobo <Zhang.Shaobo@outlook.com>
2022-08-15 11:06:28 +0800

    add a cleanup script for the template nupkg project itself

---
+++ Clean.ps1
@@ -0,0 +1,24 @@
+$foldersNeedToClean =
+".vs",
+".vscode",
+"bin",
+"obj",
+"shaobo_wasm\__PROJECT_NAME__.Client\bin",
+"shaobo_wasm\__PROJECT_NAME__.Client\obj",
+"shaobo_wasm\__PROJECT_NAME__.Server\bin",
+"shaobo_wasm\__PROJECT_NAME__.Server\obj",
+"shaobo_wasm\__PROJECT_NAME__.Shared\bin",
+"shaobo_wasm\__PROJECT_NAME__.Shared\obj";
+
+Foreach($folder in $foldersNeedToClean)
+{
+	if(Test-Path -Path $folder)
+	{
+		Write-Host "Deleting : " $folder;
+		Remove-Item -Force -Recurse -LiteralPath $folder;
+	}
+	else
+	{
+		Write-Host "Not found : " $folder;
+	}
+}
